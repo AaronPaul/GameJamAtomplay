d243f5cb8a30315396ff4d63c8687373c1dc7c27
Jannis Baudisch <jannis.baudisch@nocapes.de>
2017-08-12 19:55:29 +0200

    Sync

--- Bla/Assets/Scripts/ElectronSpawner.cs
+++ Bla/Assets/Scripts/ElectronSpawner.cs
@@ -14,10 +14,23 @@ public class ElectronSpawner : MonoBehaviour {
 	void Update () {
         if (Time.time > timeNextSpawn) {
             GameObject electron = Instantiate(electronPrefab);
-            electron.transform.position = new Vector2(100,100);
+            switch (Random.Range(0, 4)) {
+                case 0: //Top
+                    electron.transform.position = new Vector2(Random.Range(MasterHandler.playArea.edges.left, MasterHandler.playArea.edges.right), MasterHandler.playArea.edges.top);
+                    break;
+                case 1: //Bottom
+                    electron.transform.position = new Vector2(Random.Range(MasterHandler.playArea.edges.left, MasterHandler.playArea.edges.right), MasterHandler.playArea.edges.bottom);
+                    break;
+                case 2: //Left
+                    electron.transform.position = new Vector2(MasterHandler.playArea.edges.left, Random.Range(MasterHandler.playArea.edges.bottom, MasterHandler.playArea.edges.top));
+                    break;
+                case 3: //Right
+                    electron.transform.position = new Vector2(MasterHandler.playArea.edges.right, Random.Range(MasterHandler.playArea.edges.bottom, MasterHandler.playArea.edges.top));
+                    break;
+            }
             Vector2 direction = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward) * Vector3.right;
             electron.GetComponent<Electron>().init(direction);
-            timeNextSpawn = Time.time;
+            timeNextSpawn = Time.time + Random.Range(0.2f, 1f);
         }
 	}
 }
